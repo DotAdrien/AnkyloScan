9112f312d6757ae2d749e4c36ae14550baba7a7a
DotAdrien <lamoureux.adrien.29@gmail.com>
2026-01-16 12:12:17 +0100

    Update main.py

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
+import mysql.connector
 
-app = FastAPI(title="AnkyloScan API 🦖", port=8001)
+app = FastAPI(title="AnkyloScan API 🦖")
+
+# Récupère le MDP généré dans le .env ou utilise celui par défaut 🔑
+DB_PASSWORD = os.getenv("ADMIN_PASSWORD", "password_aleatoire")
 
 class ScanResult(BaseModel):
     ip: str
@@ -15,6 +20,22 @@ db_mock = []
 @app.get("/")
 def home():
     return {"message": "AnkyloScan API tournant sur le port 8001 ! 🦖🔥"}
+
+@app.get("/test-db")
+def test_db_connection():
+    try:
+        # Utilise les variables de ton docker-compose
+        connection = mysql.connector.connect(
+            host="db",
+            user="admin",
+            password=DB_PASSWORD,
+            database="ankyloscan"
+        )
+        if connection.is_connected():
+            connection.close()
+            return {"status": "success", "message": "Connexion auto réussie ! 🛡️"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erreur : {str(e)} 😱")
 
 @app.post("/results")
 def receive_results(result: ScanResult):
