c8b435b9716ec8a2d7f67ff037af62280a3fbdc4
Your Name <youremail@example.com>
2026-02-02 21:54:27 +0100

    aaaaa

---
+++ reinstall.sh
@@ -0,0 +1,18 @@
+sudo docker-compose down -v
+
+cd ..
+
+sudo rm -rf AnkyloScan
+
+# Installation of tool
+sudo apt update -y && sudo apt install -y docker.io docker-compose
+
+# Installation of AnkyloScan
+git clone https://github.com/DotAdrien/AnkyloScan
+cd AnkyloScan
+
+# Generate unique password (une seule fois) 🔑
+echo "ADMIN_PASSWORD=$(openssl rand -base64 32)" > .env
+
+# Start 🚀
+sudo docker-compose up --build
