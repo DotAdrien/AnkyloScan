4ab4d084cc7e9d3dbbd5481682ca3fd28e9f6b6f
Your Name <youremail@example.com>
2026-02-02 22:02:33 +0100

    simple

--- scanner/src/main.rs
+++ scanner/src/main.rs
@@ -1,17 +1,3 @@
-use std::net::TcpStream;
-use std::time::Duration;
-use tokio;
-
-#[tokio::main]
-async fn main() {
-    let target = "127.0.0.1";
-    println!("Démarrage du scan sur {}... 🦖", target);
-
-    for port in 1..1024 {
-        let address = format!("{}:{}", target, port);
-        if TcpStream::connect_timeout(&address.parse().unwrap(), Duration::from_millis(100)).is_ok() {
-            println!("Port {} ouvert ! 🔓", port);
-        }
-    }
-    println!("Scan terminé ! 🛡️");
+fn main() {
+    println!("je suis la 🦖✨");
 }
