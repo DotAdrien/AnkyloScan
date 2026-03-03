831a8fe8d82ee562585d7c5cc0e08341959cc3ff
dotadrien <lamoureux.adrien.29@gmail.com>
2026-03-03 14:42:41 +0100

    log

---
+++ backend/app/api/logs.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+import mysql.connector # type: ignore
+import os
+
+router = APIRouter(prefix="/logs", tags=["Logs 🛡️"])
+DB_PASSWORD = os.getenv("ADMIN_PASSWORD")
+
+class LogEntry(BaseModel):
+    event_id: int
+    source: str
+    message: str
+
+@router.post("/ingest")
+def ingest_logs(log: LogEntry):
+    conn = mysql.connector.connect(host="db", user="root", password=DB_PASSWORD, database="ankyloscan")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO SystemLogs (event_id, source, message) VALUES (%s, %s, %s)", 
+                   (log.event_id, log.source, log.message))
+    conn.commit()
+    conn.close()
+    return {"status": "Log reçu ! ✨"}
