8a56b86013bbd0d9c0b2a44ab4481f4d595e7b73
dotadrien <lamoureux.adrien.29@gmail.com>
2026-03-24 15:21:34 +0100

    sqsq

--- backend/app/secu/db.py
+++ backend/app/secu/db.py
@@ -4,7 +4,7 @@ import mysql.connector # type: ignore
 def get_db_connection():
     """Crée une connexion centralisée à la base de données 🐬"""
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "db"),
+        host=os.getenv("127.0.0.1"),
         user="root",
         password=os.getenv("ADMIN_PASSWORD"),
         database="ankyloscan"
