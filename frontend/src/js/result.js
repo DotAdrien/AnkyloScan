77a01c8893fc31f26f732e28da36850fc2fb14ed
DotAdrien <lamoureux.adrien.29@gmail.com>
2026-05-21 10:23:23 +0200

    Update result.js

--- frontend/src/js/result.js
+++ frontend/src/js/result.js
@@ -42,8 +42,8 @@ async function loadScanHistory() {
                         </button>
                     `}
                 </div>
-            `;
-        }).join('');
+            </div>
+        `).join('');
 
     } catch (error) {
         console.error(error);
